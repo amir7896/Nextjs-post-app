602c58502671b5310f247dce20fce357e2b4988c
amir7896 <79500734+amir7896@users.noreply.github.com>
2024-09-27 11:43:27 +0500

    Create topic form added

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,3 +1,51 @@
 "use server";
 
-export async function createTopic() {}
+import { z } from "zod";
+import { auth } from "@/auth";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/[a-z-]/, {
+      message: "Must be lower case letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ["You must be signed to do this."],
+      },
+    };
+  }
+  return {
+    errors: {},
+  };
+  // Todo: Revalidate the topic show page
+}
